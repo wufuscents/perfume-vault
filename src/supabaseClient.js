5936bf07a990dee2c39c2c8606d8c93fc0b5d252
wufuscents <hoomanwhomen@gmail.com>
2026-08-22 10:24:35 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://sjooywttnxvswdlziws.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqb295d3R0d254dnN3ZGx6aXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczMTI5MDksImV4cCI6MjEwMjg4ODkwOX0.Ghlc9rVrfHsq0UKaIbd4kpAQuTRpqrzTmdpOGerdUmk'
+const supabaseAnonKey = 'sb_publishable_h_PV-Lcm0fKwZNKHKaXtag_x-x2GOOU'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
